17cc21ebae6897ff366cbc2d157a5299db3b4c19
Manuel Stalder <masstalde@gmail.com>
2016-09-01 03:18:32 -0700

    Attitude estimator now functional.

--- src/swarm_attitude_node.py
+++ src/swarm_attitude_node.py
@@ -10,6 +10,8 @@ import roslib
 roslib.load_manifest('uwb')
 import rospy
 
+from numpy.linalg import inv
+
 
 import uwb.msg
 import std_msgs
@@ -21,10 +23,10 @@ class AttitudeEst(object):
         
         def __init__(self, agent_number):
             self.agent_number = agent_number;
-            vector_to_previous = [0.0, 0.0]
-            vector_to_next = [0.0, 0.0]
-            self.vectors = [vector_to_previous, vector_to_next]
-            self.master_module_offset = [0.0, 0.0]
+            vector_to_previous = np.array([0.0, 0.0])
+            vector_to_next = np.array([0.0, 0.0])
+            self.vectors = np.array([vector_to_previous, vector_to_next])
+            self.master_module_offset = np.array([0.0, 0.0])
         
 
     def __init__(self):
@@ -32,18 +34,27 @@ class AttitudeEst(object):
         self.agent_topics = []
         self.agent_list = []
         self.all_agents_init = 0
+        self.R21 = np.array([[1, 0], [0, 1]])
+        self.R31 = np.array([[1, 0], [0, 1]])
+        self.R12 = inv(self.R21)
+        self.R13 = inv(self.R31)
         
         
         for i in range(1, self.number_of_agents + 1):
             agent = self.Agent(i)
+            
+            #TEST
+            agent.master_module_offset = np.array([-0.2, 0.2])
+            
             self.agent_list.append(agent)   
             # ROS publishers
             agent_i_topic = self.agents_topic_templ.replace("*", str(i))
             agent_i_pub = rospy.Publisher(agent_i_topic, std_msgs.msg.Float32MultiArray, queue_size=3)
             self.agent_topics.append(agent_i_pub)
+            
                  
         self.tracker_sub = rospy.Subscriber(self.uwb_tracker_topic, uwb.msg.UWBTracker, self.handle_tracker_message)
-        
+              
         
     def _read_configuration(self):
         """Initialize configuration from ROS parameters."""        
@@ -64,21 +75,109 @@ class AttitudeEst(object):
         if (remote_address%self.number_of_agents) == ((agent_number - 1)%self.number_of_agents):
             agent.vectors[0] = agent_state[0:2]
             
-        self.agent_list.insert(agent_number-1, agent)   
+        self.agent_list.insert(agent_number-1, agent)
+        
         
         if self.all_agents_init:
             self.perform_kabsch()
         else:
             self.all_agents_init = 1
             for ag in self.agent_list:
-                if ag.vectors[0] == [0, 0] or ag.vectors[1] == [0, 0]:
+                if not ag.vectors[0].any() or not ag.vectors[1].any():
                     self.all_agents_init = 0
        
                
     
     def perform_kabsch(self):
          rospy.loginfo("Performing Kabsch")
-    
+         
+         #Preparing Matrices
+         P = np.array([[0, 0], self.agent_list[0].vectors[1], self.agent_list[0].vectors[0]])
+         Q21 =  np.array([self.agent_list[1].vectors[0], [0, 0], self.agent_list[1].vectors[1]])
+         Q31 =  np.array([self.agent_list[2].vectors[1], self.agent_list[2].vectors[0], [0, 0]])
+         
+         #Calculate centroids
+         P_c = self.centroid(P)
+         Q21_c = self.centroid(Q21)
+         Q31_c = self.centroid(Q31)
+         
+         P -= P_c
+         Q21 -= Q21_c
+         Q31 -= Q31_c
+         
+         self.R21 = self.kabsch(P, Q21)
+         self.R31 = self.kabsch(P, Q31)
+         
+         self.R12 = inv(self.R21)
+         self.R13 = inv(self.R31)
+           
+
+         self.publish_rotated_vectors()  
+          
+         
+    def publish_rotated_vectors(self):
+        z1 = (self.agent_list[0].vectors[1] - self.R21.dot(self.agent_list[1].vectors[0])) / 2
+        z2 = (self.R21.dot(self.agent_list[1].vectors[1]) - self.R31.dot(self.agent_list[2].vectors[0])) / 2 
+        z3 = (self.agent_list[0].vectors[0] - self.R31.dot(self.agent_list[2].vectors[1])) / 2
+        
+
+        
+        rospy.loginfo(z1)
+        rospy.loginfo(z2)
+        rospy.loginfo(z3)
+        
+     
+    def centroid(self, X):
+        """
+        Calculate the centroid from a vectorset X
+        """
+        C = sum(X)/len(X)
+        return C
+        
+    def kabsch(self, P, Q):
+        """
+        The optimal rotation matrix U is calculated and then used to rotate matrix
+        P unto matrix Q so the minimum root-mean-square deviation (RMSD) can be
+        calculated.
+        Using the Kabsch algorithm with two sets of paired point P and Q,
+        centered around the center-of-mass.
+        Each vector set is represented as an NxD matrix, where D is the
+        the dimension of the space.
+        The algorithm works in three steps:
+        - a translation of P and Q
+        - the computation of a covariance matrix C
+        - computation of the optimal rotation matrix U
+        http://en.wikipedia.org/wiki/Kabsch_algorithm
+        Parameters:
+        P -- (N, number of points)x(D, dimension) matrix
+        Q -- (N, number of points)x(D, dimension) matrix
+        Returns:
+        U -- Rotation matrix
+        """
+
+        # Computation of the covariance matrix
+        C = np.dot(np.transpose(P), Q)
+
+        # Computation of the optimal rotation matrix
+        # This can be done using singular value decomposition (SVD)
+        # Getting the sign of the det(V)*(W) to decide
+        # whether we need to correct our rotation matrix to ensure a
+        # right-handed coordinate system.
+        # And finally calculating the optimal rotation matrix U
+        # see http://en.wikipedia.org/wiki/Kabsch_algorithm
+        V, S, W = np.linalg.svd(C)
+        d = (np.linalg.det(V) * np.linalg.det(W)) < 0.0
+
+        if d:
+            S[-1] = -S[-1]
+            V[:, -1] = -V[:, -1]
+
+        # Create Rotation matrix U
+        U = np.dot(V, W)
+
+        return U
+         
+         
     def exec_(self):
         rospy.spin()
 
